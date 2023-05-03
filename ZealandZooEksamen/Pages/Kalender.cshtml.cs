023f2fca39fd3fbf717367e6fc7c70d755adf9d4
MagnusN <magnu@MAGNUS>
2023-05-03 12:00:11 +0200

    Begyndelse for kalender, plus små ændingringer på create event, der sikre at dato og tid bliver indsat korrekt (mangler sikkerhed så tidStart og tidSlut ikke bliver sat i forkert rækkefølge)

---
+++ ZealandZooEksamen/Pages/Kalender.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ZealandZooEksamen.Pages
+{
+    public class KalenderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
