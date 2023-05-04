f2b16ec5d8005d369181ad524f7163cf021c5019
MagnusN <magn077p@edu.zealand.dk>
2023-05-04 13:05:22 +0200

    Opdateringer på kalender siden

--- ZealandZooEksamen/Pages/Kalender.cshtml.cs
+++ ZealandZooEksamen/Pages/Kalender.cshtml.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ZealandZooEksamen.Model;
+using ZealandZooEksamen.Services;
 
 namespace ZealandZooEksamen.Pages
 {
     public class KalenderModel : PageModel
     {
+        private IEventService _service;
+        public KalenderModel(IEventService service)
+        {
+            _service = service;
+        }
+        public List<Event> Events { get; set; }
         public void OnGet()
         {
+            Events = _service.GetAllEvents();
         }
     }
 }
